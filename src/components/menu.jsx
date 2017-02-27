a5d358358ff25d192f85e1c8ed89171dd3a05705
Maggie.Ryder <maggie.ryder@us-nyc-C02MCDQ0FD57.local>
2017-02-27 16:31:18 -0500

    CSSModules option added

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,15 +1,16 @@
 import React, {PropTypes, Component} from 'react'
-import styles from '../css/menu.scss'
+import styles from '../css/menu.css'
 
 
 class Menu extends Component {
   render(){
     return (
-      <nav className={styles}>
+      <nav className={styles.menu}>
         <h1>Navigation</h1>
+        <h1 className={styles.red}>RedNavigation</h1>
         <ul>
           <li>one</li>
-          <li>two</li>
+          <li className={styles.red}>two</li>
           <li>three</li>
         </ul>
       </nav>
